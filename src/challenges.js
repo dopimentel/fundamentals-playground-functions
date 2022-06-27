75d8244b7068387784ca3b7ee7d073b5ec2b6a12
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-06-27 14:52:57 -0300

    Primeiro requisito - compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(parametro1, parametro2) {
+  if (parametro1 === true && parametro2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 // Desafio 2
 function calcArea() {
   // seu código aqui
