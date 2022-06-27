f02bccbf823249ffed5e2dcb6f06f7589caf7c65
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-06-27 17:04:54 -0300

    Requisito 3 - SplitSentence

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+let arrayPalavras = frase.split (" ")
+  return arrayPalavras 
 }
 
 // Desafio 4
